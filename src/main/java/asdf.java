249f02e380a6598fcef7f915d48c3b7d9ddf61ef
EvlLep <EvlLeperchaun@gmail.com>
2018-09-30 18:35:05 -0400

    asdf

---
+++ src/main/java/asdf.java
@@ -0,0 +1,29 @@
+public class asdf {
+    private String s;
+    private int i;
+    private boolean lol;
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public boolean isLol() {
+        return lol;
+    }
+
+    public void setLol(boolean lol) {
+        this.lol = lol;
+    }
+}
